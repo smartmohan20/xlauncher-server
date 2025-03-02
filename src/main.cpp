14c1150341812342377250ad2ab74325404b72ac
smartmohan20 <dev.smartmohan@gmail.com>
2025-03-02 08:05:06 +0530

    Added default paremeters for chrome app

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv) {
             "Google Chrome", 
             "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", 
             ApplicationLauncher::ApplicationType::EXECUTABLE,
-            {"google.com"}
+            {"--profile-last-used", "google.com"}
         });
 
         ApplicationLauncher::registerApplication({
